83ec61d3b84d3e084dee7456b49284f9750e510e
angeliquenordvall <angeliquenordvall@gmail.com>
2026-09-07 01:02:22 +0200

    Create primat.js

---
+++ api/primat.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://primat.nu/api/v3/demo/products?q=banan"
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+}
